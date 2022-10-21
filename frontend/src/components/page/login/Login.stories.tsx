f1307c5b576ae7cc119a8ac3a8a87a1cf29e4d7c
sg4k0 <114585573+sg4k0@users.noreply.github.com>
2022-10-21 23:21:23 +0900

    認証機能追加 #22

--- frontend/src/components/page/login/Login.stories.tsx
+++ frontend/src/components/page/login/Login.stories.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import { ComponentStoryObj, ComponentMeta } from '@storybook/react'
 import { userEvent, within } from '@storybook/testing-library'
+import { MemoryRouter as Router } from 'react-router-dom'
 
 import { Login } from './Login'
 
 export default {
   title: 'Page/Login',
-  component: Login
+  component: Login,
+  decorators: [
+    (Story) => {
+      <Router>
+        <Story />
+      </Router>
+    }
+  ]
 } as ComponentMeta<typeof Login>
 
 const Template: ComponentStoryObj<typeof Login> = () => <Login />
