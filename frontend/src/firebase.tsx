cf0d3086107ab66c31069a35a02795cf77422216
sg4k0 <114585573+sg4k0@users.noreply.github.com>
2023-06-03 22:06:21 +0900

    Emulatorの接続先を検証のため変更

--- frontend/src/firebase.tsx
+++ frontend/src/firebase.tsx
@@ -13,8 +13,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 
 export const auth = getAuth(app)
-if (import.meta.env.DEV === true) {
-  connectAuthEmulator(auth, 'http://localhost:9099')
-} else if (import.meta.env.MODE === 'test') {
-  connectAuthEmulator(auth, 'http://emulator:9099')
-}
+connectAuthEmulator(auth, 'http://localhost:9099')
+
