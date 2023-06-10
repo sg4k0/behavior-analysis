e58749a06ebb0a689ea516b37afafcee6763de7f
sg4k0 <114585573+sg4k0@users.noreply.github.com>
2023-06-11 00:21:06 +0900

    Package Upgrade

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -1,29 +1,31 @@
-const path = require('path')
-const { loadConfigFromFile, mergeConfig } = require('vite')
-
+const path = require('path');
+const {
+  loadConfigFromFile,
+  mergeConfig
+} = require('vite');
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions'
-  ],
-  framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-vite'
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {}
   },
   features: {
     storyStoreV7: true,
     interactionsDebugger: true
   },
-  async viteFinal(config, { configType }) {
-    const { config: userConfig } = await loadConfigFromFile(
-      path.resolve(__dirname, '../vite.config.ts')
-    )
-
+  async viteFinal(config, {
+    configType
+  }) {
+    const {
+      config: userConfig
+    } = await loadConfigFromFile(path.resolve(__dirname, '../vite.config.ts'));
     return mergeConfig(config, {
       ...userConfig,
       plugins: []
-    })
+    });
+  },
+  docs: {
+    autodocs: true
   }
-}
+};
--- frontend/src/components/page/login/Login.test.tsx
+++ frontend/src/components/page/login/Login.test.tsx
@@ -1,4 +1,5 @@
 import { composeStories } from '@storybook/testing-react'
+import { act } from 'react-dom/test-utils'
 import { render, screen, waitFor } from '@testing-library/react'
 import * as stories from './Login.stories'
 
@@ -12,7 +13,9 @@ describe('Login', () => {
 
   it('認証に失敗したときはAlertが表示されていること', async () => {
     const { container } = render(<AuthFailed />)
-    await AuthFailed.play({ canvasElement: container })
+    await act(async () => {
+      await AuthFailed.play({ canvasElement: container })
+    })
     await waitFor(() => {
       expect(screen.getByText('認証に失敗しました')).toBeInTheDocument
     })
@@ -20,7 +23,9 @@ describe('Login', () => {
 
   it('認証に成功したときはAlertが表示されていないこと', async () => {
     const { container } = render(<AuthSuccess />)
-    await AuthSuccess.play({ canvasElement: container })
+    await act(async () => {
+      await AuthSuccess.play({ canvasElement: container })
+    })
     await waitFor(() => {
       expect(screen.queryByText('認証に失敗しました')).not.toBeInTheDocument
     })
